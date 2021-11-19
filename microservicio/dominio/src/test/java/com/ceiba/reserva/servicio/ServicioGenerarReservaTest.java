73ea82989cd5cbe68cf7b5435954dcd6b0f6c0a5
Sebastían Girón Arcila - Ceiba Software <sebastian.giron@ceiba.com.co>
2021-11-18 23:56:17 -0500

    corrigiendo cobertura

--- microservicio/dominio/src/test/java/com/ceiba/reserva/servicio/ServicioGenerarReservaTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/reserva/servicio/ServicioGenerarReservaTest.java
@@ -16,8 +16,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ServicioGenerarReservaTest {
 
     @Test
-    @DisplayName("Deberia generar la reserva de manera correcta con un descuento del 20%")
-    void deberiaGenerarReservaDeManeraCorrectaConDescuento(){
+    @DisplayName("Deberia generar la reserva de manera correcta con un descuento del 20% cuando es martes")
+    void deberiaGenerarReservaDeManeraCorrectaConDescuentoLosMartes(){
+        //Arrange
+        DtoGlamping dtoGlamping = new GlampingTestDataBuilder().conPrecio(250000.0).buildDt();
+        Reserva reserva = new ReservaTestDataBuilder().conId(1L).conFechaRegistro(LocalDateTime.of(2021,11,16,8,30,00)).build();
+        DaoReserva daoReserva = Mockito.mock(DaoReserva.class);
+        ServicioGenerarReserva servicioGenerarReserva = new ServicioGenerarReserva(daoReserva);
+        Mockito.when(daoReserva.retonarGlampingPorId(Mockito.anyLong())).thenReturn(dtoGlamping);
+        //Act
+        Reserva reservaGenerada = servicioGenerarReserva.ejecutar(reserva);
+        //Assert
+        assertEquals(200000.0,reservaGenerada.getCostoTotal());
+
+
+    }
+
+    @Test
+    @DisplayName("Deberia generar la reserva de manera correcta con un descuento del 20% cuando es miercoles")
+    void deberiaGenerarReservaDeManeraCorrectaConDescuentoLosMiercoles(){
+        //Arrange
+        DtoGlamping dtoGlamping = new GlampingTestDataBuilder().conPrecio(250000.0).buildDt();
+        Reserva reserva = new ReservaTestDataBuilder().conId(1L).conFechaRegistro(LocalDateTime.of(2021,11,17,8,30,00)).build();
+        DaoReserva daoReserva = Mockito.mock(DaoReserva.class);
+        ServicioGenerarReserva servicioGenerarReserva = new ServicioGenerarReserva(daoReserva);
+        Mockito.when(daoReserva.retonarGlampingPorId(Mockito.anyLong())).thenReturn(dtoGlamping);
+        //Act
+        Reserva reservaGenerada = servicioGenerarReserva.ejecutar(reserva);
+        //Assert
+        assertEquals(200000.0,reservaGenerada.getCostoTotal());
+
+
+    }
+
+
+
+    @Test
+    @DisplayName("Deberia generar la reserva de manera correcta con un descuento del 20% cuando es jueves")
+    void deberiaGenerarReservaDeManeraCorrectaConDescuentoLosJueves(){
         //Arrange
         DtoGlamping dtoGlamping = new GlampingTestDataBuilder().conPrecio(250000.0).buildDt();
         Reserva reserva = new ReservaTestDataBuilder().conId(1L).conFechaRegistro(LocalDateTime.of(2021,11,18,8,30,00)).build();
@@ -32,12 +68,50 @@ public class ServicioGenerarReservaTest {
 
     }
 
+
+
+
+
     @Test
-    @DisplayName("Deberia generar la reserva de manera correcta con un aumento del 35%")
-    void deberiaGenerarReservaDeManeraCorrectaConAumento(){
+    @DisplayName("Deberia generar la reserva de manera correcta con un aumento del 35% cuando es viernes")
+    void deberiaGenerarReservaDeManeraCorrectaConAumentoLosViernes(){
         //Arrange
         DtoGlamping dtoGlamping = new GlampingTestDataBuilder().conPrecio(250000.0).buildDt();
         Reserva reserva = new ReservaTestDataBuilder().conId(1L).conFechaRegistro(LocalDateTime.of(2021,11,19,8,30,00)).build();
+        DaoReserva daoReserva = Mockito.mock(DaoReserva.class);
+        ServicioGenerarReserva servicioGenerarReserva = new ServicioGenerarReserva(daoReserva);
+        Mockito.when(daoReserva.retonarGlampingPorId(Mockito.anyLong())).thenReturn(dtoGlamping);
+        //Act
+        Reserva reservaGenerada = servicioGenerarReserva.ejecutar(reserva);
+        //Assert
+        assertEquals(337500.0,reservaGenerada.getCostoTotal());
+
+
+    }
+
+    @Test
+    @DisplayName("Deberia generar la reserva de manera correcta con un aumento del 35% cuando es sabado")
+    void deberiaGenerarReservaDeManeraCorrectaConAumentoLosSabados(){
+        //Arrange
+        DtoGlamping dtoGlamping = new GlampingTestDataBuilder().conPrecio(250000.0).buildDt();
+        Reserva reserva = new ReservaTestDataBuilder().conId(1L).conFechaRegistro(LocalDateTime.of(2021,11,20,8,30,00)).build();
+        DaoReserva daoReserva = Mockito.mock(DaoReserva.class);
+        ServicioGenerarReserva servicioGenerarReserva = new ServicioGenerarReserva(daoReserva);
+        Mockito.when(daoReserva.retonarGlampingPorId(Mockito.anyLong())).thenReturn(dtoGlamping);
+        //Act
+        Reserva reservaGenerada = servicioGenerarReserva.ejecutar(reserva);
+        //Assert
+        assertEquals(337500.0,reservaGenerada.getCostoTotal());
+
+
+    }
+
+    @Test
+    @DisplayName("Deberia generar la reserva de manera correcta con un aumento del 35% cuando es domingo")
+    void deberiaGenerarReservaDeManeraCorrectaConAumentoLosDomingos(){
+        //Arrange
+        DtoGlamping dtoGlamping = new GlampingTestDataBuilder().conPrecio(250000.0).buildDt();
+        Reserva reserva = new ReservaTestDataBuilder().conId(1L).conFechaRegistro(LocalDateTime.of(2021,11,21,8,30,00)).build();
         DaoReserva daoReserva = Mockito.mock(DaoReserva.class);
         ServicioGenerarReserva servicioGenerarReserva = new ServicioGenerarReserva(daoReserva);
         Mockito.when(daoReserva.retonarGlampingPorId(Mockito.anyLong())).thenReturn(dtoGlamping);
